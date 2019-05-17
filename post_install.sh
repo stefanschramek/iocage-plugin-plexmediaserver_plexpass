69286c3ec29de55f601cd99f1cbd0ad65781dcc5
Stefan Schramek <stefanschramek@users.noreply.github.com>
2019-05-17 21:49:42 +0200

    Create post_install.sh

---
+++ post_install.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# Enable the service
+sysrc -f /etc/rc.conf plexmediaserver_plexpass_enable="YES"
+
+#Try to make filesystem readable
+sysrc -f /etc/rc.conf plexmediaserver_plexpass_support_path="/config"
+
+# Start the service
+service plexmediaserver_plexpass start 2>/dev/null
