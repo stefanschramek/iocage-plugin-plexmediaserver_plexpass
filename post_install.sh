398ad7bbf4c300de3b0fa08874ad6897c89434cf
Stefan Schramek <stefanschramek@users.noreply.github.com>
2019-12-24 15:42:10 +0100

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -3,7 +3,7 @@
 pw groupadd -n mnt -g 1002
 pw groupmod 1002 -m 972
 
-sysrc -f /etc/rc.conf plexmediaserver_plexpass_support_path="/config"
+sysrc -f /etc/rc.conf plexmediaserver_plexpass_support_path="/"
 sysrc -f /etc/rc.conf plexmediaserver_plexpass_enable="YES"
 
 #service plexmediaserver_plexpass start 2>/dev/null
