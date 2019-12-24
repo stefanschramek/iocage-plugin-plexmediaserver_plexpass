ef612ba8907a335e82b261aa0b3fda2348fa0dee
Stefan Schramek <stefanschramek@users.noreply.github.com>
2019-12-24 15:10:40 +0100

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
-# Enable the service
+pw groupadd -n mnt -g 1002
+pw groupmod 1002 -m 972
+
+sysrc -f /etc/rc.conf plexmediaserver_plexpass_support_path="/config"
 sysrc -f /etc/rc.conf plexmediaserver_plexpass_enable="YES"
 
-#Try to make filesystem readable
-sysrc -f /etc/rc.conf plexmediaserver_plexpass_support_path="/config"
+#service plexmediaserver_plexpass start 2>/dev/null
 
-# Start the service
-service plexmediaserver_plexpass start 2>/dev/null
+echo "Plex Media Server successfully installed" > /root/PLUGIN_INFO
