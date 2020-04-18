453cce482ba9aa84da7b6852463da7481856b5c3
Stefan Schramek <stefanschramek@users.noreply.github.com>
2020-04-18 14:50:34 +0200

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
-pw groupadd -n mnt -g 1002
-pw groupmod 1002 -m 972
+sysrc -f /etc/rc.conf plexmediaserver_plexpass_support_path="/usr/local/plexdata-plexpass"
+# Configure tmp directory used for the transcoding process
+sysrc -f /etc/rc.conf plexmediaserver_plexpass_tmp="/var/tmp/plex"
 
-sysrc -f /etc/rc.conf plexmediaserver_plexpass_support_path="/"
+# Enable the service
 sysrc -f /etc/rc.conf plexmediaserver_plexpass_enable="YES"
 
+# Start the service
 #service plexmediaserver_plexpass start 2>/dev/null
 
 echo "Plex Media Server successfully installed" > /root/PLUGIN_INFO
